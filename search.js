b871d80a32f9da0765dcfe16b8eb9c7f69077e2e
anorneto <anornetoo@gmail.com>
2019-11-23 19:27:54 -0300

    interactivity added  😁

---
+++ search.js
@@ -0,0 +1,43 @@
+//<script async src="https://cse.google.com/cse.js?cx=008344922520587658943:kunfp2xcyyo"></script>
+
+/* function search(querryItem, date) {
+  var url = `https://cors-anywhere.herokuapp.com/https://www.googleapis.com/customsearch/v1?q=${querryItem}&cx=008344922520587658943%3Akunfp2xcyyo&key=AIzaSyDMq1Fk8N8GORYadfLGY_bBQTYbjE5-rKw`;
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", url, true);
+
+  xhttp.onreadystatechange = function() {
+    //Função a ser chamada quando a requisição retornar do servidor
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      //Verifica se o retorno do servidor deu certo
+      console.log('Retorno Ok, Json a seguir');
+      var jsonParsed = JSON.parse(xhttp.response);
+      console.log(jsonParsed);
+      document.getElementById(
+        "card-search"
+      ).innerHTML = `<p>${jsonParsed.items[0].htmlSnippet}</p>
+      <p> Link : ${jsonParsed.items[0].link}</p>
+      `;
+    }
+  };
+
+  xhttp.send();
+} */
+
+async function search(querryItem, date) {
+  var afterDate = new Date(date);
+  var beforeDate = new Date(date);
+  afterDate.setDate(afterDate.getDate() - 1);
+  beforeDate.setDate(beforeDate.getDate() +1 );
+  var afterDateString = afterDate.toISOString();
+  afterDateString = afterDateString.substring(0,afterDateString.indexOf('T'));
+  var beforeDateString = beforeDate.toISOString();
+  beforeDateString = beforeDateString.substring(0,beforeDateString.indexOf('T'));
+
+  var url = `https://cors-anywhere.herokuapp.com/https://www.googleapis.com/customsearch/v1?q=${querryItem} after:${afterDateString} before:${beforeDateString}&cx=008344922520587658943%3Akunfp2xcyyo&key=AIzaSyDMq1Fk8N8GORYadfLGY_bBQTYbjE5-rKw`;
+
+  const response = await fetch(url);
+  const jsonParsed = await response.json();
+  return jsonParsed;
+}
+
